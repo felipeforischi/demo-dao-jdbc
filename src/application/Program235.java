60dc70f7b9b51000a564362afa80027497b1e92f
felipeforischi <felipecorreio77@gmail.com>
2019-12-15 08:27:53 -0300

    insert implementation

--- src/application/Program235.java
+++ src/application/Program235.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
@@ -33,6 +34,10 @@ public class Program235 {
 			System.out.println(obj);
 		}
 		
+		System.out.println("=== TEST 4 : seller Insert ===");
+		Seller newSeller = new Seller (null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserido! Novo id = " + newSeller.getId());
 		/*
 		System.out.println("=== TEST 6: seller delete ====");
 		System.out.println("Digite o id para deletar: ");
